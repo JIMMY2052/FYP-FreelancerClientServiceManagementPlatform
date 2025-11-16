07751942aa87297d9a1e750023575785f184808c
Jimmy Chan <jimmychankahlok66@gmail.com>
2025-11-16 23:41:49 +0800

    update the message

--- page/messages.php
+++ page/messages.php
@@ -149,7 +149,8 @@ $user_email = $_SESSION['email'] ?? '';
                     <div class="input-wrapper">
                         <button type="button" class="input-action-btn" id="attachBtn" title="Attach files">
                             <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
-                                <path d="M21.5 2v6h-6M2.5 22v-6h6M2 11.5a10 10 0 0 0 19.8-4.3M22 12.5a10 10 0 0 0-19.8 4.2"></path>
+                                <path d="M10 13a5 5 0 0 0 7.54.54l3-3a5 5 0 0 0-7.07-7.07l-1.72 1.71"></path>
+                                <path d="M14 11a5 5 0 0 0-7.54-.54l-3 3a5 5 0 0 0 7.07 7.07l1.71-1.71"></path>
                             </svg>
                         </button>
                         <input
